e14e405fa7bf36e5701404803eae076faf9bcc98
blrsh <30329602+blrsh@users.noreply.github.com>
2017-07-20 17:49:07 -0600

    Full File

---
+++ PBM/Ball.js
@@ -0,0 +1,26 @@
+function Ball(id){
+  //positioning
+  this.x = -20;
+  this.y = -20;
+  this.xspeed = 0;
+  this.yspeed = 0;
+  this.speed = 10;
+  this.wid = 10;
+  this.id = id;
+  
+  //color
+  var c = [0, 0, 0]
+  if(id == 1){
+    c = [255, 0, 51]
+  } else if(id == 2){
+    c = [22, 141, 255]
+  }
+  
+  this.show = function(){
+    fill(c[0], c[1], c[2]);
+    ellipse(this.x, this.y, this.wid, this.wid);
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+  }
+  
+}
